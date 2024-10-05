9069bf2a98670a94e21709e4737bb748ee09d1fa
link <bjhhopesin@gmail.com>
2024-10-05 20:55:46 +0900

    타슈켄트 위치 셋팅

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-import './style.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import "./style.css";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import { fromLonLat, Projection } from "ol/proj";
+import OSM from "ol/source/OSM";
 
 const map = new Map({
-  target: 'map',
+  target: "map",
   layers: [
     new TileLayer({
-      source: new OSM()
-    })
+      source: new OSM(),
+    }),
   ],
   view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+    center: fromLonLat([69.24843722816026, 41.31653827549364]),
+    zoom: 14,
+  }),
 });
