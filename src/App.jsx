40f29714f7dca931e8414214958e4f1f27fb9fba
link <bjhhopesin@gmail.com>
2024-10-06 16:03:24 +0900

    go to center

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
@@ -7,6 +7,10 @@ import { fromLonLat } from "ol/proj";
 
 function App() {
   const mapRef = useRef(null);
+
+  const [map, setMap] = useState(null);
+  const [lon, setLon] = useState(127.12);
+  const [lat, setLat] = useState(37.41);
 
   useEffect(() => {
     const map = new Map({
@@ -22,21 +26,47 @@ function App() {
       }),
     });
 
+    setMap(map);
     return () => {
       map.setTarget(null);
     };
   }, []);
 
+  const moveMap = () => {
+    if (map) {
+      const newCenter = fromLonLat([parseFloat(lon), parseFloat(lat)]);
+      map.getView().setCenter(newCenter);
+    }
+  };
+
   return (
-    <div
-      ref={mapRef}
-      style={{
-        position: "absolute",
-        top: 0,
-        bottom: 0,
-        width: "100%",
-      }}
-    />
+    <div>
+      <div style={{ marginTop: "20px" }}>
+        <label htmlFor="longitude">Longitude: </label>
+        <input
+          type="text"
+          id="longitude"
+          value={lon}
+          onChange={(e) => setLon(e.target.value)}
+        />
+        <label htmlFor="latitude">Latitude: </label>
+        <input
+          type="text"
+          id="latitude"
+          value={lat}
+          onChange={(e) => setLat(e.target.value)}
+        />
+        <button onClick={moveMap}>Go to Location</button>
+      </div>
+
+      <div
+        ref={mapRef}
+        style={{
+          height: "400px",
+          width: "100%",
+        }}
+      />
+    </div>
   );
 }
 
